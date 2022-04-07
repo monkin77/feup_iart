cc2f116b0ae78525d872dc1e23185ebbaaee6b86
Monkin <monkinsmurf@gmail.com>
2022-04-07 11:43:28 +0100

    Added solutions logs to verify intermediate solutions

--- src/Solution.py
+++ src/Solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def __init__(self, intersections):
         self.intersections = intersections
-    
+
     def setInitialSolution(self):
         for intersection in self.intersections:
             intersection.changeAllSemaphores(1)
@@ -11,3 +11,13 @@ class Solution:
             intersection.incomingStreets = [
                 street for street in intersection.incomingStreets if street[0].carUsageCount != 0
             ]
+
+    def show(self):
+        for intersection in self.intersections:
+            print("|| Intersection " + str(intersection.id) + " ||")
+            semString = "["
+            for (street, semaphore) in intersection.incomingStreets:
+                semString += str(semaphore) + ", "
+            semString = semString[:-2]  # remove last 2 characters
+            semString += "]"
+            print(semString)
