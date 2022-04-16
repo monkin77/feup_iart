d980fc7791b3903338dcb4f9717185c700581f27
up201905853 <up201905853@fe.up.pt>
2022-04-16 17:36:24 +0100

    Improved menus and get all parameters from user

--- src/main.py
+++ src/main.py
@@ -8,22 +8,26 @@ INPUT_FOLDER = "../input/"
 OUTPUT_FOLDER = "../output/"
 
 files = {1: 'a.txt', 2: 'b.txt', 3: 'c.txt', 4: 'd.txt', 5: 'e.txt', 6: 'f.txt'}
+flags = {'removeUnusedStreets': False, 'iterations': 0, 'inicialTemperature': 100, 'alpha': 0.85, 'precision': 10, 'tabuNumCandidates': 10}
 
-def runAlgorithm(inputFile, outputFile, algorithmInput):
+def runAlgorithm(inputFile, outputFile, algorithmInput, flags):
     (intersections, cars, maxTime, bonusPoints) = readInput(inputFile)
     simulation = Simulation(cars, maxTime, bonusPoints)
     solution = Solution(intersections, simulation)
-    solution.removeUnusedStreets()
+
+    if flags['removeUnusedStreets']:
+        solution.removeUnusedStreets()
+
     solution.setInitialSolution()
 
     if algorithmInput == 1:
-        finalScore = solution.hillClimbingBasicRandom(1)
+        finalScore = solution.hillClimbingBasicRandom(flags['iterations'])
     elif algorithmInput == 2:
         finalScore = solution.hillClimbingSteepest()
     elif algorithmInput == 3:
-        finalScore = solution.simulatedAnnealing(100, 0.85, 10, exponentialCooling)
+        finalScore = solution.simulatedAnnealing(flags['inicialTemperature'], flags['alpha'], flags['precision'], exponentialCooling)
     elif algorithmInput == 4:
-        finalScore = solution.tabuSearch(100, 10)
+        finalScore = solution.tabuSearch(flags['iterations'], flags['tabuNumCandidates'])
 
     solution.show()
     print("Final score = ", finalScore)
@@ -34,6 +38,7 @@ def program():
     currentMenu = 0
     inputFile = INPUT_FOLDER + 'a.txt'
     outputFile = OUTPUT_FOLDER + "out.txt"
+    currFlags = flags
 
     while currentMenu != -1:
         if currentMenu == 0:  # main menu
@@ -48,18 +53,44 @@ def program():
                 currentMenu = 2
 
             inputFile = INPUT_FOLDER + files[fileOption]
-
-        elif currentMenu == 2:
+        elif currentMenu == 2:    
+            configInput = getInitialConfig()
+            userInput = True
+            # choose algorithm
+            if configInput == 8:
+                currentMenu = 3
+            # exit
+            elif configInput == 9: 
+                currentMenu = 0
+                continue
+            else:
+                key = getConfigKeyFromInput(configInput)
+                if key == 'removeUnusedStreets':
+                    userInput = True 
+                elif key == 'addUnusedStreets':
+                    key = 'removeUnusedStreets'
+                    userInput = False 
+                else:
+                    userInput = getNumberInput(0)
+                currFlags[key] = userInput
+                
+        elif currentMenu == 3:
             algorithmInput = showAlgorithmMenu()
-            # if it's 6 then choose another file
+
+            # choose another file
             if algorithmInput == 6:
                 currentMenu = 1
                 continue
+            # new configurations
             elif algorithmInput == 7:
+                currentMenu = 2
+            # go to main menu
+            elif algorithmInput == 8:
                 currentMenu = 0
                 continue
+            else:
+                runAlgorithm(inputFile, outputFile, algorithmInput, flags)
 
-            runAlgorithm(inputFile, outputFile, algorithmInput)
 
 
 if __name__ == "__main__":
--- src/menus.py
+++ src/menus.py
@@ -12,6 +12,19 @@ def getInput(minValue, maxValue):
 
     return val
 
+def getNumberInput(minValue):
+    userInput = input("Insert new value: ")
+    while True:
+        try:
+            val = int(userInput)
+            if val < minValue:
+                userInput = input("Invalid new value, please insert a positive one: ")
+            else:
+                break
+        except ValueError:
+            userInput = input("Invalid new value, please insert a positive one: ")
+
+    return val
 
 def printMenu(options):
     print("\n***************************************")
@@ -44,7 +57,34 @@ def showAlgorithmMenu():
         4: 'Tabu Search', 
         5: 'Genetic', 
         6: 'Choose another file',
-        7: 'Go to Main Menu',
+        7: 'Set New Configurations',
+        8: 'Go to Main Menu',
     }
     return printMenu(options)
 
+
+def getInitialConfig():
+    options = {
+        1: 'Remove Unused Streets       ------      (All)',
+        2: 'Add Unused Streets          ------      (All)',
+        3: 'Set Max Num Iterations      ------      (Hill Climbing Basic Random | Tabu Search)',
+        4: 'Set Inicial Temperature     ------      (Simulated Annealing)',
+        5: 'Set Alpha                   ------      (Simulated Annealing)',
+        6: 'Set precision               ------      (Simulated Annealing)',
+        7: 'Tabu Number of Candidates   ------      (Tabu Search)',
+        8: 'Choose Algorithm',
+        9: 'Go to Main Menu',
+    }
+    return printMenu(options)
+
+def getConfigKeyFromInput(input):
+    options = {
+        1: 'removeUnusedStreets',
+        2: 'addUnusedStreets',
+        3: 'iterations',
+        4: 'inicialTemperature',
+        5: 'alpha',
+        6: 'precision',
+        7: 'tabuNumCandidates',
+    }
+    return options[input]
